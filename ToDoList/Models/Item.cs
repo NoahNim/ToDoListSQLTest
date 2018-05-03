5b2f99dcd0410a13ba05691819a44db5b47083a3
Guest User <Guest@epicodus-21.local>
2018-05-03 14:38:42 -0700

    create edit function

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -137,5 +137,34 @@ namespace ToDoList.Models
            conn.Dispose();
        }
     }
+
+    public void Edit(string newDescription)
+    {
+        MySqlConnection conn = DB.Connection();
+        conn.Open();
+        var cmd = conn.CreateCommand() as MySqlCommand;
+        cmd.CommandText = @"UPDATE items SET description = @newDescription WHERE id = @searchId;";
+
+        MySqlParameter searchId = new MySqlParameter();
+        searchId.ParameterName = "@searchId";
+        searchId.Value = _id;
+        cmd.Parameters.Add(searchId);
+
+        MySqlParameter description = new MySqlParameter();
+        description.ParameterName = "@newDescription";
+        description.Value = newDescription;
+        cmd.Parameters.Add(description);
+
+        cmd.ExecuteNonQuery();
+        _description = newDescription;
+
+        conn.Close();
+        if (conn != null)
+        {
+            conn.Dispose();
+        }
+    }
+
+
   }
 }
