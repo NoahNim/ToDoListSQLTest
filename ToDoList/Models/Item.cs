3f0f0422f7c96843812ad8707ddba5eac4d3c973
Guest User <Guest@epicodus-21.local>
2018-05-03 09:18:18 -0700

    create test folder and test for database

---
+++ ToDoList/Models/Item.cs
@@ -0,0 +1,81 @@
+using System.Collections.Generic;
+using MySql.Data.MySqlClient;
+using ToDoList;
+using System;
+
+namespace ToDoList.Models
+{
+  public class Item
+  {
+    private string _description;
+    private int _id;
+
+    public Item (string Description, int Id = 0)
+    {
+      _description = Description;
+      _id = Id;
+    }
+    public string GetDescription()
+    {
+      return _description;
+    }
+    public void SetDescription(string newDescription)
+    {
+      _description = newDescription;
+    }
+    public int GetId()
+    {
+      return _id;
+    }
+    public static List<Item> GetAll()
+    {
+      List<Item> allItems = new List<Item> {};
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+      MySqlCommand cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"SELECT * FROM items;";
+      MySqlDataReader rdr = cmd.ExecuteReader() as MySqlDataReader;
+      while(rdr.Read())
+        {
+          int itemId = rdr.GetInt32(0);
+          string itemDescription = rdr.GetString(1);
+          Item newItem = new Item(itemDescription, itemId);
+          allItems.Add(newItem);
+        }
+        conn.Close();
+        if (conn != null)
+        {
+          conn.Dispose();
+        }
+        return allItems;
+    }
+    // public void Save()
+    // {
+    //   _instances.Add(this);
+    // }
+    //     public static void ClearAll()
+    // {
+    //   _instances.Clear();
+    // }
+    // public static Item Find(int searchId)
+    // {
+    //   return _instances[searchId-1];
+    // }
+    public static void DeleteAll()
+   {
+       MySqlConnection conn = DB.Connection();
+       conn.Open();
+
+       var cmd = conn.CreateCommand() as MySqlCommand;
+       cmd.CommandText = @"DELETE FROM items;";
+
+       cmd.ExecuteNonQuery();
+
+       conn.Close();
+       if (conn != null)
+       {
+           conn.Dispose();
+       }
+    }
+  }
+}
